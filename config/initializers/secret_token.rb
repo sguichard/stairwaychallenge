c5bcc9bf2f2c4e2f09cc375c7efc1655a78138af
Steven Guichard <ssguichard@gmail.com>
2013-03-31 16:33:25 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stairchallenge::Application.config.secret_token = '1705deac3827343db11d983e5b5081613acb54252c52dce07be797f372f8759711367c88cc5c9b77a08005ebe7a8e2640ffe32a36a8350ac276092d2df4184ac'
