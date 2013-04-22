a85063826f9b464b755bb88eb6d2c97373a3d023
Steven Guichard <ssguichard@gmail.com>
2013-04-21 18:56:47 -0700

    Added sorting by name and number of stairs

--- app/controllers/stairways_controller.rb
+++ app/controllers/stairways_controller.rb
@@ -1,11 +1,20 @@
 class StairwaysController < ApplicationController
   protect_from_forgery
+  helper_method :sort_column, :sort_direction
   
   def index
-    @stairways = Stairway.all.sort_by { |s| s.stairs || 0 }.reverse
+    @stairways = Stairway.order(sort_column + ' ' + sort_direction)
     @json = Stairway.all.to_gmaps4rails do |stairway, marker|
-    marker.infowindow render_to_string(:partial => "/stairways/infowindow", :locals => { :stairway => stairway})
-      
+      marker.infowindow render_to_string(:partial => "/stairways/infowindow", :locals => { :stairway => stairway})
     end
+  end
+
+  private
+  def sort_column
+    Stairway.column_names.include?(params[:sort]) ? params[:sort] : 'name'
+  end
+
+  def sort_direction
+    ['asc', 'desc'].include?(params[:direction]) ? params[:direction] : 'asc'
   end
 end
